dcd81bfa761f89211a412c562178d731ef4405a7
Ville K <test@hopo.com>
2023-01-03 15:04:44 +0200

    async and listing

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get("/test", (req, res) => {
-    res.send("Number1: " +  String(Math.round(Math.random() * 2 + 1)) + "<br>" +
-            "Number2: " +  String(Math.round(Math.random() * 2 + 1)) + "<br>" +
-            "Number3: " +  String(Math.round(Math.random() * 2 + 1)) + "<br>"
-    );
+  res.send(
+    "Number1: " +
+      String(Math.round(Math.random() * 2 + 1)) +
+      "<br>" +
+      "Number2: " +
+      String(Math.round(Math.random() * 2 + 1)) +
+      "<br>" +
+      "Number3: " +
+      String(Math.round(Math.random() * 2 + 1)) +
+      "<br>"
+  );
+});
+
+app.get("/locations", (req, res) => {
+  let database = [
+    { id: 1, latitude: 60, longitude: 70 },
+    { id: 2, latitude: 40, longitude: 80 },
+  ];
+  res.json(database);
 });
 
 app.listen(port, () => {
